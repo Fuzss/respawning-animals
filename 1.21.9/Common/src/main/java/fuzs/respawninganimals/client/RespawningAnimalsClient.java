4f10a131f49959eaf199693f172f8a318c3af46d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 22:20:31 +0200

    prepare 1.21.10 port

--- 1.21.9/Common/src/main/java/fuzs/respawninganimals/client/RespawningAnimalsClient.java
+++
@@ -1,7 +0,0 @@
-package fuzs.respawninganimals.client;
-
-import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
-
-public class RespawningAnimalsClient implements ClientModConstructor {
-
-}
